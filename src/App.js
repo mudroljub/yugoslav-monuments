df8dcebb148a3cd2394f0c3c1b70fc8f596482fe
Damjan Pavlica <mudroljub@gmail.com>
2018-05-05 01:09:50 +0200

    redux popravljen

--- src/App.js
+++ src/App.js
@@ -7,62 +7,16 @@ import {
   TouchableOpacity
 } from "react-native"
 import MapView from "react-native-maps"
+import {connect} from 'react-redux'
 
 import styles from './styles.js'
 
-export default class App extends Component {
-  state = {
-    monuments: [
-      {
-        coordinate: {
-          latitude: 45.524548,
-          longitude: -122.6749817,
-        },
-        title: "Best Place",
-        description: "This is the best place in Portland",
-        image: { uri: "https://i.imgur.com/sNam9iJ.jpg" },
-      },
-      {
-        coordinate: {
-          latitude: 45.524698,
-          longitude: -122.6655507,
-        },
-        title: "Second Best Place",
-        description: "This is the second best place in Portland",
-        image: { uri: "https://i.imgur.com/N7rlQYt.jpg" },
-      },
-      {
-        coordinate: {
-          latitude: 45.5230786,
-          longitude: -122.6701034,
-        },
-        title: "Third Best Place",
-        description: "This is the third best place in Portland",
-        image: { uri: "https://i.imgur.com/UDrH0wm.jpg" },
-      },
-      {
-        coordinate: {
-          latitude: 45.521016,
-          longitude: -122.6561917,
-        },
-        title: "Fourth Best Place",
-        description: "This is the fourth best place in Portland",
-        image: { uri: "https://i.imgur.com/Ka8kNST.jpg" },
-      },
-    ],
-    region: {
-      latitude: 43.0602992,
-      longitude: 18.5701971,
-      latitudeDelta: 0.3, // zoom
-      longitudeDelta: 0.3,
-    },
-  }
-
+class App extends Component {
   centerMap = marker => {
     this.map.animateToRegion({
       ...marker.coordinate,
-      latitudeDelta: this.state.region.latitudeDelta,
-      longitudeDelta: this.state.region.longitudeDelta,
+      latitudeDelta: this.props.region.latitudeDelta,
+      longitudeDelta: this.props.region.longitudeDelta,
     }, 350)
   }
 
@@ -92,10 +46,10 @@ export default class App extends Component {
       <View style={styles.container}>
         <MapView
           ref={map => this.map = map}
-          initialRegion={this.state.region}
+          initialRegion={this.props.region}
           style={styles.container}
         >
-          {this.state.monuments.map(this.renderMarker)}
+          {this.props.monuments.map(this.renderMarker)}
         </MapView>
 
         <ScrollView
@@ -104,9 +58,15 @@ export default class App extends Component {
           style={styles.scrollView}
           contentContainerStyle={styles.endPadding}
         >
-          {this.state.monuments.map(this.renderCard)}
+          {this.props.monuments.map(this.renderCard)}
         </ScrollView>
       </View>
     )
   }
 }
+
+const mapStateToProps = ({region, monuments}) => ({
+  region, monuments
+})
+
+export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Expo from 'expo'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
-import {reducer} from './store'
+import {reducer} from './state'
 import App from './App'
 
 const store = createStore(reducer)
