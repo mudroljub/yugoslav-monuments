c0a15608826a017d3cfe134af60f9c922628a53d
Damjan Pavlica <mudroljub@gmail.com>
2018-05-05 14:17:15 +0200

    separate data json

--- src/state/index.js
+++ src/state/index.js
@@ -1,3 +1,5 @@
+import monuments from '../data/monuments.json'
+
 const initialState = {
   region: {
     latitude: 43.0602992,
@@ -5,44 +7,7 @@ const initialState = {
     latitudeDelta: 0.3, // zoom
     longitudeDelta: 0.3,
   },
-  monuments: [
-    {
-      coordinate: {
-        latitude: 45.524548,
-        longitude: -122.6749817,
-      },
-      title: "Best Place",
-      description: "This is the best place in Portland",
-      image: { uri: "https://i.imgur.com/sNam9iJ.jpg" },
-    },
-    {
-      coordinate: {
-        latitude: 45.524698,
-        longitude: -122.6655507,
-      },
-      title: "Second Best Place",
-      description: "This is the second best place in Portland",
-      image: { uri: "https://i.imgur.com/N7rlQYt.jpg" },
-    },
-    {
-      coordinate: {
-        latitude: 45.5230786,
-        longitude: -122.6701034,
-      },
-      title: "Third Best Place",
-      description: "This is the third best place in Portland",
-      image: { uri: "https://i.imgur.com/UDrH0wm.jpg" },
-    },
-    {
-      coordinate: {
-        latitude: 45.521016,
-        longitude: -122.6561917,
-      },
-      title: "Fourth Best Place",
-      description: "This is the fourth best place in Portland",
-      image: { uri: "https://i.imgur.com/Ka8kNST.jpg" },
-    },
-  ],
+  monuments
 }
 
 export const reducer = (state = initialState, action) => {
