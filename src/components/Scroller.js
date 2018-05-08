2a9f7fd1830512883334f373dea67ab2cf9f5d94
Damjan Pavlica <mudroljub@gmail.com>
2018-05-08 15:35:24 +0200

    toggle cards

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import {ScrollView} from 'react-native'
+import React, {Component} from 'react'
+import {ScrollView, Text, View} from 'react-native'
 import {connect} from 'react-redux'
 
 import Card from './Card'
 import styles from '../styles'
 
-const Scroller = ({monuments}) => (
-  <ScrollView horizontal
-    style={styles.scrollView}
-  >
-    {monuments
-      .filter(marker => marker.image)
-      .map((marker, i) => <Card key={i} marker={marker} />
-    )}
-  </ScrollView>
-)
+class Scroller extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {hidden: false}
+  }
+
+  render() {
+    return (
+      <View style={styles.scrollView}>
+        <Text style={styles.bigSize}
+          onPress={() => this.setState({hidden: !this.state.hidden})}
+        >&#9650;</Text>
+        <ScrollView horizontal style={this.state.hidden && {display: 'none'}}>
+          {this.props.monuments
+            .filter(marker => marker.image)
+            .map((marker, i) => <Card key={i} marker={marker}/>)
+          }
+        </ScrollView>
+      </View>
+    )
+  }
+}
 
 const mapStateToProps = ({monuments}) => ({monuments})
 
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
-import {StyleSheet, Dimensions} from "react-native"
+import {StyleSheet, Dimensions} from 'react-native'
 
-const { width, height } = Dimensions.get("window")
+const { height } = Dimensions.get('window')
 const CARD_HEIGHT = height / 4
 const CARD_WIDTH = CARD_HEIGHT - 50
 
@@ -8,14 +8,12 @@ export default StyleSheet.create({
   container: {
     flex: 1,
   },
-  geolocation: {
-    top: 40,
-    position: "absolute",
-    left: 20,
-    fontSize: 26,
+  bigSize: {
+    fontSize: 52,
+    left: 10,
   },
   scrollView: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 5,
     left: 0,
     right: 0,
@@ -24,29 +22,29 @@ export default StyleSheet.create({
   card: {
     padding: 10,
     elevation: 2,
-    backgroundColor: "#FFF",
+    backgroundColor: '#FFF',
     marginHorizontal: 10,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowRadius: 5,
     shadowOpacity: 0.3,
     shadowOffset: { x: 2, y: -2 },
     height: CARD_HEIGHT,
     width: CARD_WIDTH,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
   cardImage: {
     flex: 3,
-    width: "100%",
-    height: "100%",
-    alignSelf: "center",
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
   },
   cardtitle: {
     fontSize: 12,
     marginTop: 5,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   cardDescription: {
     fontSize: 12,
-    color: "#444",
+    color: '#444',
   },
 })
